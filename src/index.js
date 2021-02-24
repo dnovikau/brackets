9afa4e538a9d6c0518077f733effe764b40e8876
Daniil Novikau <novikaud@gmail.com>
2021-02-24 11:41:19 +0300

    Task: https://github.com/Shastel/brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = '';
+  bracketsConfig.forEach((conf) => {
+    brackets += `${conf.map((i) => i.match(/\d/) ? i : `\\${i}`).join('')}|`;
+  });
+
+  brackets = brackets.slice(0, -1);
+  const regexp = new RegExp(brackets, 'g')
+
+  while (regexp.test(str)) {
+    str = str.replace(regexp, '');
+  }
+  return !str;
 }
